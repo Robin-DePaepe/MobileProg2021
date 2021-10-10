3d928410c2895d8422cb618f324871aa068a83c1
Lowskii <37476424+Robin-DePaepe@users.noreply.github.com>
2021-10-11 02:48:47 +0300

    first adds implementation

---
+++ ProgMobile2021/Assets/W40 Ads/AdsManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOsGameId;
+    [SerializeField] bool _testMode = true;
+    [SerializeField] bool _enablePerPlacementMode = true;
+
+    //reward
+    [SerializeField] Button _showAdButton;
+   string _androidAdUnitId = "Rewarded_Android";
+   string _iOsAdUnitId = "Rewarded_iOS";
+    //interstitial 
+    //string _androidAdUnitId = "Interstitial_Android";
+    //string _iOsAdUnitId = "Interstitial_iOS";
+    string _adUnitId;
+
+    private string _gameId;
+
+    void Awake()
+    {
+        InitializeAds();
+
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+    ? _iOsAdUnitId
+    : _androidAdUnitId;
+
+        //Disable button until ad is ready to show
+        _showAdButton.interactable = false;
+
+        Invoke("LoadAd", 3f);
+    }
+
+    public void InitializeAds()
+    {
+        Advertisement.AddListener(this);
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, _enablePerPlacementMode, this);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+
+    // Load content to the Ad Unit:
+    public void LoadAd()
+    {
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
+    }
+
+    // Show the loaded content in the Ad Unit: 
+    public void ShowAd()
+    {
+        // Disable the button: 
+        _showAdButton.interactable = false;
+
+        // Note that if the ad content wasn't previously loaded, this method will fail
+        Debug.Log("Showing Ad: " + _adUnitId);
+        Advertisement.Show(_adUnitId);
+    }
+
+    // Implement Load Listener and Show Listener interface methods:  
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+
+        if (adUnitId.Equals(_adUnitId))
+        {
+            // Configure the button to call the ShowAd() method when clicked:
+            _showAdButton.onClick.AddListener(ShowAd);
+            // Enable the button for users to click:
+            _showAdButton.interactable = true;
+        }
+    }
+
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
+        // Optionally execite code if the Ad Unit fails to load, such as attempting to try again.
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Optionally execite code if the Ad Unit fails to show, such as loading another ad.
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { Debug.Log("why u clicking?"); }
+
+
+    void OnDestroy()
+    {
+        // Clean up the button listeners:
+        _showAdButton.onClick.RemoveAllListeners();
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (placementId.Equals("Rewarded_Android") && showResult.Equals(ShowResult.Finished))
+        {
+            Debug.Log("Unity Ads Rewarded Ad Completed");
+            // Grant a reward.
+            CoinManager.Instance.AddCurrency(100);
+            // Load another ad:
+            Advertisement.Load(_adUnitId, this);
+        }
+    }
+}
